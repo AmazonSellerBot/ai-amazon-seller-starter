26727a17201903884e6a4837cc0b71475353c301
AmazonSellerBot <craig@FoodCrush.io>
2025-06-28 10:52:30 -0700

    Update test_spapi.py

--- test_spapi.py
+++ test_spapi.py
@@ -36,7 +36,7 @@ def get_marketplace_participations(access_token):
     SigV4Auth(credentials, "execute-api", "us-east-1").add_auth(aws_request)
 
     session = requests.Session()
-    response = session.send(aws_request.prepare())
+    response = session.send(aws_request)
     response.raise_for_status()
     return response.json()
 
