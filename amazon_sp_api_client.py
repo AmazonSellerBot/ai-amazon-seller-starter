b45dd4e8c52d44868e678c376b169220999a8a71
AmazonSellerBot <craig@FoodCrush.io>
2025-06-30 21:13:56 -0700

    Update amazon_sp_api_client.py

--- amazon_sp_api_client.py
+++ amazon_sp_api_client.py
@@ -3,6 +3,7 @@ from sp_api.api import Listings
 from sp_api.base import Marketplaces, SellingApiException
 from sp_api.base.credentials import Credentials
 
+
 class AmazonSPAPIClient:
     def __init__(self):
         self.credentials = Credentials(
