8b431a60ba57d8cad73d59090b17dd6b5b85486f
AmazonSellerBot <craig@FoodCrush.io>
2025-07-02 14:35:49 -0700

    Update amazon_sp_api_client.py

--- amazon_sp_api_client.py
+++ amazon_sp_api_client.py
@@ -1,21 +1,21 @@
-import os
-from dotenv import load_dotenv
-from sp_api.api.listings import Listings
-from sp_api.base import SellingApiCredentials, Marketplaces
+from sp_api.api.catalog import Catalog
+from sp_api.base import Marketplaces, SellingApiCredentials
 
-load_dotenv()
+import os
 
 class AmazonSPAPIClient:
     def __init__(self):
-        self.credentials = SellingApiCredentials(
-            refresh_token=os.getenv("SPAPI_REFRESH_TOKEN"),
-            lwa_app_id=os.getenv("SPAPI_CLIENT_ID"),
-            lwa_client_secret=os.getenv("SPAPI_CLIENT_SECRET"),
-            aws_secret_key=os.getenv("SPAPI_AWS_SECRET_ACCESS_KEY"),
-            aws_access_key=os.getenv("SPAPI_AWS_ACCESS_KEY_ID"),
-            role_arn=os.getenv("SPAPI_ROLE_ARN")
+        self.catalog = Catalog(
+            credentials=SellingApiCredentials(
+                refresh_token=os.getenv("LWA_REFRESH_TOKEN"),
+                lwa_app_id=os.getenv("LWA_APP_ID"),
+                lwa_client_secret=os.getenv("LWA_CLIENT_SECRET"),
+                aws_secret_key=os.getenv("SPAPI_AWS_SECRET_ACCESS_KEY"),
+                aws_access_key=os.getenv("SPAPI_AWS_ACCESS_KEY_ID"),
+                role_arn=os.getenv("SPAPI_ROLE_ARN"),
+            ),
+            marketplace=Marketplaces.US
         )
 
-    def get_my_listings(self):
-        res = Listings(credentials=self.credentials, marketplace=Marketplaces.US).get_listings_items(sellerId=os.getenv("SELLER_ID"))
-        return res.payload
+    def get_catalog_item(self, asin):
+        return self.catalog.get_catalog_item(asin).payload
