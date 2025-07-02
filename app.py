ace72d57d719e0f6a0b1056014887a7ff89e23d0
AmazonSellerBot <craig@FoodCrush.io>
2025-07-02 15:53:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-from amazon_sp_api_client import AmazonSPAPIClient
 
 app = FastAPI()
-client = AmazonSPAPIClient()
 
 @app.get("/")
 def read_root():
     return {"message": "Amazon Seller Starter is running!"}
-
-@app.get("/catalog/{asin}")
-def get_catalog_info(asin: str):
-    return client.get_product_by_asin(asin)
