8c60af603dd2c148442a2f142e1013b1017a5dd8
AmazonSellerBot <craig@FoodCrush.io>
2025-07-04 11:49:14 -0700

    Update amazon_sp_api_client.py

--- amazon_sp_api_client.py
+++ amazon_sp_api_client.py
@@ -1,20 +1,21 @@
 import os
 from sp_api.api import Products
-from sp_api.base import SellingApiCredentials, Marketplaces
-
-credentials = SellingApiCredentials(
-    refresh_token=os.getenv("LWA_REFRESH_TOKEN"),
-    lwa_app_id=os.getenv("LWA_CLIENT_ID"),
-    lwa_client_secret=os.getenv("LWA_CLIENT_SECRET"),
-    aws_secret_key=os.getenv("SPAPI_AWS_SECRET_ACCESS_KEY"),
-    aws_access_key=os.getenv("SPAPI_AWS_ACCESS_KEY_ID"),
-    role_arn=os.getenv("SPAPI_ROLE_ARN"),
-)
+from sp_api.base import Marketplaces
 
 def get_product_pricing(asin: str):
     try:
-        products = Products(credentials=credentials, marketplace=Marketplaces.US)
-        response = products.get_product_pricing(ItemType='ASIN', ItemIds=[asin])
+        products = Products(
+            marketplace=Marketplaces.US,
+            credentials=dict(
+                refresh_token=os.environ["LWA_REFRESH_TOKEN"],
+                lwa_app_id=os.environ["LWA_CLIENT_ID"],
+                lwa_client_secret=os.environ["LWA_CLIENT_SECRET"],
+                aws_access_key=os.environ["SPAPI_AWS_ACCESS_KEY_ID"],
+                aws_secret_key=os.environ["SPAPI_AWS_SECRET_ACCESS_KEY"],
+                role_arn=os.environ["SPAPI_ROLE_ARN"],
+            )
+        )
+        response = products.get_product_pricing(ItemType="ASIN", ItemIds=[asin])
         return response.payload
     except Exception as e:
         return {"error": str(e)}
