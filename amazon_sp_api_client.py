f7a89c8f83c5d5ca8586fd53c1aee4e308f3dee9
AmazonSellerBot <craig@FoodCrush.io>
2025-07-04 23:52:37 -0700

    Update amazon_sp_api_client.py

--- amazon_sp_api_client.py
+++ amazon_sp_api_client.py
@@ -1,21 +1,6 @@
-import os
-from sp_api.api import Products
-from sp_api.base import Marketplaces
-
 def get_product_pricing(asin: str):
-    try:
-        products = Products(
-            marketplace=Marketplaces.US,
-            credentials=dict(
-                refresh_token=os.environ["LWA_REFRESH_TOKEN"],
-                lwa_app_id=os.environ["LWA_CLIENT_ID"],
-                lwa_client_secret=os.environ["LWA_CLIENT_SECRET"],
-                aws_access_key=os.environ["SPAPI_AWS_ACCESS_KEY_ID"],
-                aws_secret_key=os.environ["SPAPI_AWS_SECRET_ACCESS_KEY"],
-                role_arn=os.environ["SPAPI_ROLE_ARN"],
-            )
-        )
-        response = products.get_product_pricing(ItemType="ASIN", ItemIds=[asin])
-        return response.payload
-    except Exception as e:
-        return {"error": str(e)}
+    return {
+        "asin": asin,
+        "mock_price": "$19.99",
+        "status": "dummy response - success"
+    }
