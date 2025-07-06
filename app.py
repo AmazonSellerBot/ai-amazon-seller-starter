730081eef126006766f76b05f40442afca225280
AmazonSellerBot <craig@FoodCrush.io>
2025-07-05 23:35:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,3 @@ def root():
 @app.post("/pricing")
 def pricing(data: PricingRequest):
     return get_product_pricing(data.asin)
-
-if __name__ == "__main__":
-    import uvicorn
-    print("ENV PORT:", os.environ.get("PORT"))
-    uvicorn.run("app:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
