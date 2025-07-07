d410c082fdd79ef6926c7c5263856397bf67f883
AmazonSellerBot <craig@FoodCrush.io>
2025-07-07 15:17:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from amazon_sp_api_client import get_product_pricing
-import os
 
 app = FastAPI()
 
@@ -15,8 +14,3 @@ def root():
 @app.post("/pricing")
 def pricing(data: PricingRequest):
     return get_product_pricing(data.asin)
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))  # ✅ Convert to int
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
